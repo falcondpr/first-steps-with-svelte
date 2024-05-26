f748cd083ec502e4e5ae735000384eb2ab930009
Fernando Villalba <21fervillalbag@gmail.com>
2024-05-25 21:31:32 -0400

    hello-world and global styles

---
+++ src/components/Header.svelte
@@ -0,0 +1,7 @@
+<h1>Header</h1>
+
+<style>
+	:global(body) {
+		font-family: sans-serif;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	let name = 'Fernando';
+
+	import Header from '../components/Header.svelte';
+</script>
+
+<Header />
+<h1>Welcome to {name} in Svelte Library</h1>
+
+<style>
+</style>
